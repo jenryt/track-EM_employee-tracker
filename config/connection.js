f9ba7202c0d59da55fc645f4eb235b417f641809
Jen Ryt <jenyjlin@gmail.com>
2023-06-01 16:00:22 -0700

    add-fn and update-fn

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,11 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 // Create a new MySQL connection object
-const connection = mysql
-  .createConnection({
-    host: process.env.HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  })
-  .promise();
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 module.exports = connection;
