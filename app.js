2275c342c2aacf5bfc857ab80c5694d7990c9556
Jen Ryt <jenyjlin@gmail.com>
2023-06-01 13:06:28 -0700

    view all function setup

--- app.js
+++ app.js
@@ -1,17 +1,84 @@
 const inquirer = require("inquirer");
-const express = require("express");
 const connection = require("./config/connection");
+const cTable = require("console.table");
+const {
+  viewAllDepartments,
+  viewAllRoles,
+  viewAllEmployees,
+} = require("./utils/queries");
 
-const app = express();
+// // Create a function to initialize app
+// function init() {
+//   inquirer
+//     .prompt([
+//       {
+//         type: "list",
+//         message: "What would you like to do?",
+//         name: "todo",
+//         choices: [
+//           "View All Departments",
+//           "View All Roles",
+//           "View All Employees",
+//           "Add Department",
+//           "Add Role",
+//           "Add An Employee",
+//           "Update Employee Role",
+//           "Quit",
+//         ],
+//       },
+//     ])
+//     .then(async (response) => {
+//       switch (response.todo) {
+//         case "View All Departments":
+//           viewAllDepartments();
+//           break;
 
-const PORT = process.env.PORT || 3001;
+//         case "View All Roles":
+//           viewAllRoles();
+//           break;
 
-// middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//         case "View All Employees":
+//           viewAllEmployees();
+//           break;
 
-// Create a function to initialize app
+//         case "Add Department":
+//           break;
+
+//         case "Add Role":
+//           break;
+
+//         case "Add An Employee":
+//           break;
+
+//         case "Update Employee Role":
+//           break;
+
+//         case "Quit":
+//           process.exit();
+//       }
+//     });
+// }
+
+// // Function call to initialize app
+// init();
+
+// // .then(() => {
+// //   inquirer.prompt([
+// //     {
+// //       type: "confirm",
+// //       message: "continue?",
+// //       name: "continue",
+// //     },
+// //   ]);
+// // });
+
+const pastSelections = [];
+
 function init() {
+  promptUser();
+}
+
+function promptUser() {
   inquirer
     .prompt([
       {
@@ -31,32 +98,42 @@ function init() {
       },
     ])
     .then(async (response) => {
-      switch (response.todo) {
-        case "View All Departments": {
+      const selected = response.todo;
+
+      switch (selected) {
+        case "View All Departments":
+          viewAllDepartments();
           break;
-        }
-        case "View All Roles": {
+
+        case "View All Roles":
+          viewAllRoles();
           break;
-        }
-        case "View All Employees": {
+
+        case "View All Employees":
+          viewAllEmployees();
           break;
-        }
-        case "Add Department": {
+
+        case "Add Department":
           break;
-        }
-        case "Add Role": {
+
+        case "Add Role":
           break;
-        }
-        case "Add An Employee": {
+
+        case "Add An Employee":
           break;
-        }
-        case "Update Employee Role": {
+
+        case "Update Employee Role":
           break;
-        }
-        case "Quit": {
-          return;
-        }
+
+        case "Quit":
+          // console.log("Past selections:", pastSelections);
+          process.exit();
       }
+
+      pastSelections.push(selected); // Add the selected option to past selections
+
+      console.log(); // Add a newline for better readability
+      promptUser(); // Prompt user again for the next selection
     });
 }
 
